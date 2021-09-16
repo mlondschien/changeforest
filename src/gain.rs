016212c4127c347f54acacd7fc8f24b19fecd08f
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-09-16 16:53:47 +0200

    Add gain (#3)
    * Add Gain from Loss.
    
    * Make GainFromLoss public.
    
    * fmt.
    
    * Clippy.

---
+++ src/gain.rs
@@ -0,0 +1,48 @@
+use super::loss;
+use ndarray;
+
+pub trait Gain {
+    fn gain(&self, X: &ndarray::Array2<f64>, start: usize, stop: usize, split: usize) -> f64;
+}
+
+pub struct GainFromLoss {
+    loss: Box<dyn loss::Loss>,
+}
+
+impl Gain for GainFromLoss {
+    fn gain(&self, X: &ndarray::Array2<f64>, start: usize, stop: usize, split: usize) -> f64 {
+        self.loss.loss(X, start, stop) // X already is a reference here (?)
+            - self.loss.loss(X, start, split)
+            - self.loss.loss(X, split, stop)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use assert_approx_eq::*;
+    use rstest::*;
+
+    #[rstest]
+    #[case(0, 4, 2, 0.25)]
+    #[case(0, 4, 0, 0.)]
+    #[case(0, 4, 1, 1. / 12.)]
+    #[case(0, 4, 3, 1. / 12.)]
+    #[case(0, 3, 2, 1. / 6.)]
+    #[case(0, 3, 1, 1. / 24.)]
+    fn test_change_in_mean_gain(
+        #[case] start: usize,
+        #[case] stop: usize,
+        #[case] split: usize,
+        #[case] expected: f64,
+    ) {
+        let X = ndarray::array![[0., 0.], [0., 0.], [0., 1.], [0., 1.]];
+        assert_eq!(X.shape(), &[4, 2]);
+
+        let gain = GainFromLoss {
+            loss: Box::new(loss::ChangeInMeanLoss {}),
+        };
+        assert_approx_eq!(gain.gain(&X, start, stop, split), expected);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // Allow capital X for arrays.
 #![allow(non_snake_case)]
 
+pub mod gain;
 pub mod loss;
