2b64054310f067341168139c6548afbe22507753
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-09-21 17:00:52 +0200

    Implement smarter version of `ChangeInMean::gain` using cumsum (#7)
    * Merge gain and loss into single trait.
    
    * Also merge optimizer.
    
    * Add lifetimes and smarter ChangeInMean gain.
    
    * Some hacking to make the tests pass.
    
    * Let's get rid of the hack.
    
    * Also compare to direct computation.
    
    * Get rid of '1. +' leftover from debugging.
    
    * Some more brevity.
    
    * clippy.

--- src/gain.rs
+++ src/gain.rs
@@ -1,16 +1,16 @@
 use ndarray;
 
-pub trait Segment<'a> {
+pub trait Segment {
     #[allow(unused_variables)]
-    fn loss(&'a self, start: usize, stop: usize) -> f64 {
+    fn loss(&self, start: usize, stop: usize) -> f64 {
         panic!("Not implemented.");
     }
 
-    fn gain(&'a self, start: usize, stop: usize, split: usize) -> f64 {
+    fn gain(&mut self, start: usize, stop: usize, split: usize) -> f64 {
         self.loss(start, stop) - self.loss(start, split) - self.loss(split, stop)
     }
 
-    fn find_best_split(&'a self, start: usize, stop: usize) -> usize {
+    fn find_best_split(&mut self, start: usize, stop: usize) -> usize {
         let mut max_index = 0;
         let mut max_value = -f64::INFINITY;
         let mut gain: f64;
@@ -28,10 +28,30 @@ pub trait Segment<'a> {
 
 pub struct ChangeInMean<'a> {
     X: &'a ndarray::Array2<f64>,
+    X_cumsum: Option<ndarray::Array2<f64>>,
 }
 
-impl<'a> Segment<'a> for ChangeInMean<'a> {
-    fn loss(&'a self, start: usize, stop: usize) -> f64 {
+impl<'a> ChangeInMean<'a> {
+    #[allow(dead_code)] // TODO Get rid of this.
+    fn new(X: &'a ndarray::Array2<f64>) -> ChangeInMean<'a> {
+        ChangeInMean {
+            X,
+            X_cumsum: Option::None,
+        }
+    }
+
+    fn calculate_cumsum(&mut self) {
+        let mut X_cumsum = ndarray::Array2::zeros((self.X.nrows() + 1, self.X.ncols()));
+        let mut slice = X_cumsum.slice_mut(ndarray::s![1.., ..]);
+        slice += &self.X.view();
+
+        X_cumsum.accumulate_axis_inplace(ndarray::Axis(0), |&prev, curr| *curr += prev);
+        self.X_cumsum = Some(X_cumsum);
+    }
+}
+
+impl<'a> Segment for ChangeInMean<'a> {
+    fn loss(&self, start: usize, stop: usize) -> f64 {
         if start == stop {
             return 0.;
         };
@@ -48,6 +68,30 @@ impl<'a> Segment<'a> for ChangeInMean<'a> {
             - slice.sum_axis(ndarray::Axis(0)).mapv(|a| a.powi(2)).sum() / n_slice;
 
         loss / n_total
+    }
+
+    fn gain(&mut self, start: usize, stop: usize, split: usize) -> f64 {
+        if (start == split) | (split == stop) {
+            return 0.;
+        }
+
+        if self.X_cumsum.is_none() {
+            self.calculate_cumsum();
+        }
+
+        let X_cumsum = self.X_cumsum.as_ref().unwrap();
+
+        let s_1 = (split - start) as f64;
+        let s_2 = (stop - split) as f64;
+        let s = s_1 + s_2;
+
+        let mut result = 0.;
+        for idx in 0..self.X.ncols() {
+            result += (s_1 * X_cumsum[[stop, idx]] + s_2 * X_cumsum[[start, idx]]
+                - s * X_cumsum[[split, idx]])
+            .powi(2)
+        }
+        result / (s * s_1 * s_2 * (self.X.nrows() as f64))
     }
 }
 
@@ -69,7 +113,7 @@ mod tests {
         let X = ndarray::array![[0., 0.], [0., 0.], [0., 1.], [0., 1.]];
         assert_eq!(X.shape(), &[4, 2]);
 
-        let change_in_mean = ChangeInMean { X: &X };
+        let change_in_mean = ChangeInMean::new(&X);
         assert_approx_eq!(change_in_mean.loss(start, stop), expected);
     }
 
@@ -86,11 +130,27 @@ mod tests {
         #[case] split: usize,
         #[case] expected: f64,
     ) {
-        let X = ndarray::array![[0., 0.], [0., 0.], [0., 1.], [0., 1.]];
+        let X = ndarray::array![[1., 0.], [1., 0.], [1., 1.], [1., 1.]];
         assert_eq!(X.shape(), &[4, 2]);
 
-        let change_in_mean = ChangeInMean { X: &X };
+        let mut change_in_mean = ChangeInMean::new(&X);
         assert_approx_eq!(change_in_mean.gain(start, stop, split), expected);
+        assert_approx_eq!(
+            change_in_mean.loss(start, stop)
+                - change_in_mean.loss(start, split)
+                - change_in_mean.loss(split, stop),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_X_cumsum() {
+        let X = ndarray::array![[1., 0.], [1., 0.], [1., 1.], [1., 1.]];
+        let mut change_in_mean = ChangeInMean::new(&X);
+        change_in_mean.calculate_cumsum();
+
+        let expected = ndarray::array![[0., 0.], [1., 0.], [2., 0.], [3., 1.], [4., 2.]];
+        assert_eq!(change_in_mean.X_cumsum.unwrap(), expected);
     }
 
     #[rstest]
@@ -107,7 +167,7 @@ mod tests {
         let X = ndarray::array![[0.], [0.], [1.], [1.], [-1.], [-1.], [-1.]];
         assert_eq!(X.shape(), &[7, 1]);
 
-        let change_in_mean = ChangeInMean { X: &X };
+        let mut change_in_mean = ChangeInMean::new(&X);
 
         assert_eq!(change_in_mean.find_best_split(start, stop), expected);
     }
