82eeccc22469432830cf923c75cf551cdd45a859
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-09-22 10:28:30 +0200

    Use minimal segment length in optimizer (#10)
    * Teach minimal segment length to optimizer.
    
    * Fix stack overflow.
    
    * Add utility function to get split points from BinarySegmentationTree.
    
    * clippy.

--- src/binary_segmentation.rs
+++ src/binary_segmentation.rs
@@ -28,13 +28,19 @@ impl BinarySegmentationTree {
     }
 
     fn grow(&mut self, optimizer: &mut impl optimizer::Optimizer) {
-        if self.control.minimal_relative_segment_length * (self.n as f64)
-            >= 2. * (self.stop - self.start) as f64
-        {
+        let minimal_segment_length =
+            (self.control.minimal_relative_segment_length * (self.n as f64)).round() as usize;
+
+        if 2 * minimal_segment_length >= (self.stop - self.start) {
             return;
         }
 
-        let best_split = optimizer.find_best_split(self.start, self.stop);
+        let split_candidates =
+            (self.start + minimal_segment_length)..(self.stop - minimal_segment_length);
+
+        assert!(!split_candidates.is_empty());
+
+        let best_split = optimizer.find_best_split(self.start, self.stop, split_candidates);
 
         let mut left = Box::new(BinarySegmentationTree {
             start: self.start,
@@ -61,6 +67,17 @@ impl BinarySegmentationTree {
         self.split = Some(best_split);
         self.left = Some(left);
         self.right = Some(right);
+    }
+
+    fn split_points(&self) -> Vec<usize> {
+        if let Some(split_point) = self.split {
+            let out = self.left.as_ref().unwrap().split_points().into_iter();
+            let out = out.chain(vec![split_point].into_iter());
+            let out = out.chain(self.right.as_ref().unwrap().split_points().into_iter());
+            out.collect()
+        } else {
+            return vec![];
+        }
     }
 }
 
@@ -90,15 +107,18 @@ mod tests {
 
         assert_eq!(X.shape(), &[100, 5]);
 
-        let mut optimizer = optimizer::ChangeInMean::new(&X);
         let control = control::Control {
             minimal_relative_segment_length: 0.1,
         };
+        let mut optimizer = optimizer::ChangeInMean::new(&X);
         let mut binary_segmentation = BinarySegmentationTree::new(&X, control);
 
         binary_segmentation.grow(&mut optimizer);
 
         assert_eq!(binary_segmentation.split, Some(25));
-        assert_eq!(binary_segmentation.right.unwrap().split, Some(40));
+        assert_eq!(
+            binary_segmentation.split_points(),
+            vec![14, 25, 40, 56, 69, 80]
+        );
     }
 }
