a76d3b9b8a2bf9d07e9444770408babec42e6837
Malte Londschien <malte@londschien.de>
2021-09-30 16:56:35 +0200

    Black and isort.

--- hdcd-python/tests/test_hdcd.py
+++ hdcd-python/tests/test_hdcd.py
@@ -1,14 +1,17 @@
-import pytest
 from pathlib import Path
+
 import numpy as np
+import pytest
 from hdcdpython import hdcd
 
 _IRIS_FILE = "iris.csv"
-_IRIS_PATH = Path(__file__).resolve().parents[1] / "testdata" / _IRIS_FILE
+_IRIS_PATH = Path(__file__).resolve().parents[2] / "testdata" / _IRIS_FILE
+
 
 @pytest.fixture()
 def iris_dataset():
-    return np.loadtxt("../testdata/iris.csv", skiprows=1, delimiter=",", usecols=(0, 1, 2, 3))
+    return np.loadtxt(_IRIS_PATH, skiprows=1, delimiter=",", usecols=(0, 1, 2, 3))
+
 
 def test_hdcd(iris_dataset):
     result = hdcd(iris_dataset)
