d57dfbd6a2a3e42ce6bcc630a8f0c01ebd2437a8
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2021-10-01 08:32:27 +0200

    Add python (#18)
    * Add python API.
    
    * Add ci.
    
    * Add failing test.
    
    * Revert.
    
    * Black and isort.

---
+++ hdcd-python/src/lib.rs
@@ -0,0 +1,13 @@
+use hdcd::wrapper;
+use numpy::PyReadonlyArray2;
+use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
+
+#[allow(non_snake_case)] // Allow capital X for arrays.
+#[pymodule]
+fn hdcdpython(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m)]
+    fn hdcd<'py>(X: PyReadonlyArray2<'py, f64>) -> PyResult<Vec<usize>> {
+        Ok(wrapper::hdcd(&X.as_array()))
+    }
+    Ok(())
+}
