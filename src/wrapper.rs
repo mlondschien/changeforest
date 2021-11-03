37aff4f47ea6add4d03dcfe62bbb8007b27d4a55
Malte Londschien <malte@londschien.de>
2021-11-03 08:38:19 +0100

    Clippedy clip.

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -28,10 +28,7 @@ pub fn hdcd(
     if method == "knn" {
         let classifier = kNN::new(X);
         let gain = ClassifierGain { classifier };
-        let optimizer = TwoStepSearch {
-            gain,
-            control: &control,
-        };
+        let optimizer = TwoStepSearch { gain, control };
         let mut segmentation = Segmentation::new(segmentation_type_enum, &optimizer);
         tree = BinarySegmentationTree::new(X);
         tree.grow(&mut segmentation);
@@ -39,20 +36,14 @@ pub fn hdcd(
     } else if method == "random_forest" {
         let classifier = RandomForest::new(X);
         let gain = ClassifierGain { classifier };
-        let optimizer = TwoStepSearch {
-            gain,
-            control: &control,
-        };
+        let optimizer = TwoStepSearch { gain, control };
         let mut segmentation = Segmentation::new(segmentation_type_enum, &optimizer);
         tree = BinarySegmentationTree::new(X);
         tree.grow(&mut segmentation);
         BinarySegmentationResult::from_tree(tree)
     } else if method == "change_in_mean" {
         let gain = ChangeInMean::new(X);
-        let optimizer = GridSearch {
-            gain,
-            control: &control,
-        };
+        let optimizer = GridSearch { gain, control };
         let mut segmentation = Segmentation::new(segmentation_type_enum, &optimizer);
         tree = BinarySegmentationTree::new(X);
         tree.grow(&mut segmentation);
