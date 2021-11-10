3e1be6a500ef083358b6cd7f6b44f6d8e20a3a5d
Malte Londschien <malte@londschien.de>
2021-11-10 14:01:01 +0100

    Tests.

---
+++ hdcd-python/tests/conftest.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import numpy as np
+import pytest
+
+_IRIS_FILE = "iris.csv"
+_IRIS_PATH = Path(__file__).resolve().parents[2] / "testdata" / _IRIS_FILE
+
+@pytest.fixture(scope="module")
+def iris_dataset():
+    return np.loadtxt(_IRIS_PATH, skiprows=1, delimiter=",", usecols=(0, 1, 2, 3))
--- hdcd-python/tests/test_hdcd.py
+++ hdcd-python/tests/test_hdcd.py
@@ -1,20 +1,9 @@
-from pathlib import Path
-
-import numpy as np
 import pytest
-from hdcdpython import hdcd
-
-_IRIS_FILE = "iris.csv"
-_IRIS_PATH = Path(__file__).resolve().parents[2] / "testdata" / _IRIS_FILE
-
-
-@pytest.fixture()
-def iris_dataset():
-    return np.loadtxt(_IRIS_PATH, skiprows=1, delimiter=",", usecols=(0, 1, 2, 3))
+from hdcd import hdcd
+import numpy as np
 
 @pytest.mark.parametrize("method", ["knn", "change_in_mean", "random_forest"])
 @pytest.mark.parametrize("segmentation_type", ["sbs", "wbs", "bs"])
 def test_hdcd(iris_dataset, method, segmentation_type):
     result = hdcd(iris_dataset, method, segmentation_type)
     np.testing.assert_array_equal(result.split_points(), [50, 100])
-    
