96d7e80564e3e16e1673f18e2e2f172e5f7e039d
Malte Londschien <malte@londschien.de>
2021-11-15 11:30:26 +0100

    Use gain from first iter in TwoStepSearch for classifier based model selection.

--- hdcd-python/src/lib.rs
+++ hdcd-python/src/lib.rs
@@ -3,7 +3,7 @@ mod result;
 
 use crate::control::control_from_pyobj;
 use crate::result::MyBinarySegmentationResult;
-use hdcd::{wrapper, Control};
+use hdcd::wrapper;
 use numpy::PyReadonlyArray2;
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 use pyo3::PyObject;
