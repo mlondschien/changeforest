24b7af5d6673ee13fd89244e5a176c07b0756ffb
Malte Londschien <malte@londschien.de>
2021-12-10 07:42:00 +0100

    Fix R tests.

--- changeforest-r/tests/testthat/test-control.R
+++ changeforest-r/tests/testthat/test-control.R
@@ -4,11 +4,11 @@ test_that("control", {
     X_iris = get_iris()
 
     # minimal_relative_segment_length
-    expect_lists_equal(changeforest(X, "knn", "bs", Control$new(minimal_relative_segment_length=0.05))$split_points(), c(2, 3, 5,7, 8))
+    expect_lists_equal(changeforest(X, "knn", "bs", Control$new(minimal_relative_segment_length=0.05))$split_points(), c(3, 5))
     expect_lists_equal(changeforest(X, "knn", "bs", Control$new(minimal_relative_segment_length=0.4))$split_points(), c(5))
 
     # minimal_gain_to_split
-    expect_lists_equal(changeforest(X, "change_in_mean", "bs", Control$new(minimal_gain_to_split=0.1))$split_points(), c(3, 5, 8))
+    expect_lists_equal(changeforest(X, "change_in_mean", "bs", Control$new(minimal_gain_to_split=0.1))$split_points(), c(3, 5))
     expect_lists_equal(changeforest(X, "change_in_mean", "bs", Control$new(minimal_gain_to_split=0.2))$split_points(), c(5))
     expect_lists_equal(changeforest(X, "change_in_mean", "bs", Control$new(minimal_gain_to_split=1))$split_points(), c())
 
@@ -17,10 +17,10 @@ test_that("control", {
     expect_lists_equal(changeforest(X_iris, "random_forest", "bs", Control$new(model_selection_alpha=0.05))$split_points(), c(50, 100))
 
     # seeded_segments_alpha
-    expect_equal(length(changeforest(X, "change_in_mean", "bs")$segments), 5)
+    expect_equal(length(changeforest(X, "change_in_mean", "bs")$segments), 4)
     expect_equal(length(changeforest(X_iris, "change_in_mean", "sbs", Control$new(minimal_relative_segment_length=0.05, seeded_segments_alpha=1/sqrt(2)))$segments), 44 + 5)
     expect_equal(length(changeforest(X_iris, "change_in_mean", "sbs", Control$new(minimal_relative_segment_length=0.05, seeded_segments_alpha=1/2))$segments), 25 + 5)
-    expect_equal(length(changeforest(X, "change_in_mean", "sbs", Control$new(minimal_relative_segment_length=0.05, seeded_segments_alpha=1/2))$segments), 10 + 5)
+    expect_equal(length(changeforest(X, "change_in_mean", "sbs", Control$new(minimal_relative_segment_length=0.05, seeded_segments_alpha=1/2))$segments), 10 + 4)
 
     # number_of_wild_segments
     expect_equal(length(changeforest(X_iris, "change_in_mean", "wbs", Control$new(number_of_wild_segments=10))$segments), 10 + 5)
@@ -35,7 +35,9 @@ test_that("control", {
     expect_equal(result$segments[[1]]$max_gain, 45.43954, tolerance=1e-5)
 
     # random_forest_ntree
-    expect_lists_equal(changeforest(X_iris, "random_forest", "bs", Control$new(random_forest_ntree=1))$split_points(), c(48, 98))
+    expect_lists_equal(changeforest(X_iris, "random_forest", "bs", Control$new(random_forest_ntree=1))$split_points(), c(50, 100))
     expect_lists_equal(changeforest(X_iris, "random_forest", "bs", Control$new(random_forest_ntree=100))$split_points(), c(50, 100))
+    expect_lists_equal(changeforest(X, "random_forest", "bs", Control$new(random_forest_ntree=1))$split_points(), c())
+    expect_lists_equal(changeforest(X, "random_forest", "bs", Control$new(random_forest_ntree=10))$split_points(), c(5))
 })
 
