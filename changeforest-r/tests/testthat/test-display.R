ef894259cca1f8dc017e122655ce34d5360b95e1
Malte Londschien <malte@londschien.de>
2022-03-17 10:52:26 +0100

    R tests.

--- changeforest-r/tests/testthat/test-display.R
+++ changeforest-r/tests/testthat/test-display.R
@@ -6,11 +6,11 @@ test_that("display", {
 
     expected = c(
         "             name best_split max_gain p_value is_significant",
-        "1 (0, 10]                  5 8.095522    0.01           TRUE",
-        "2  ¦--(0, 5]               3 3.459535    0.01           TRUE",
-        "3  ¦    ¦--(0, 3]          1 0.000000    1.00          FALSE",
+        "1 (0, 10]                  5 8.095522   0.005           TRUE",
+        "2  ¦--(0, 5]               3 3.459535   0.005           TRUE",
+        "3  ¦    ¦--(0, 3]          1 0.000000   1.000          FALSE",
         "4  ¦    °--(3, 5]         NA       NA      NA          FALSE",
-        "5  °--(5, 10]              7 1.383814    0.57          FALSE"
+        "5  °--(5, 10]              7 1.383814   0.610          FALSE"
     )
 
     expect_lists_equal(output, expected)
